0df7b0ad5827b452ce1401983673d870d67fbce9
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-23 21:49:09 +0300

    this commit creates a class User and generates a docstring for it

---
+++ user-test.py
@@ -0,0 +1,7 @@
+import unittest
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.Testcase: TestCase class that helps in creating text cases
+    '''
