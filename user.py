5a0d090503a5feebb0144d47b719e47555964c18
RichieOrito <richard.omondi@student.moringaschool.com>
2022-01-23 22:02:24 +0300

    this commit creates the constructor and passes in arguments

--- user.py
+++ user.py
@@ -2,3 +2,13 @@ class User:
     '''
     Class that generates new instances of contacts
     '''
+def __init__(self,first_name,last_name,user_name,passowrd):
+    '''
+    __init__ method that helps us define properties for our objects.
+
+    Args:
+        first_name: New user first name.
+        last_name : New user last name.
+        user_name: New user user_name.
+        password: New user password.
+    '''
